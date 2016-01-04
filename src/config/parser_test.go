270fc1b0ffb01fec8795ce86ffad33ed6ece9de4
blackgold <adevendra23@gmail.com>
2016-01-03 18:43:18 -0800

    add dir parsing

--- src/config/parser_test.go
+++ src/config/parser_test.go
@@ -16,4 +16,19 @@ func TestParse(t *testing.T) {
         if conf.Algorithm.Name != "randomized" {
                 t.Error("Expected randomized and got " + conf.Algorithm.Name )
         }
+}
+
+func TestParseDir(t *testing.T) {
+        confArray, err := config.ParseDir("../../config")
+        if err != nil {
+                t.Error("Expected nil and got error " + err.Error() )
+        }
+	for _, conf := range *confArray {
+            if conf.Servicename != "file" {
+                t.Error("Expected file and got " + conf.Servicename )
+            }
+            if conf.Algorithm.Name != "randomized" {
+                t.Error("Expected randomized and got " + conf.Algorithm.Name )
+            }
+	}
 }  
